e3ee0b8a1107dad7e211d43f7470190b9d227aa4
Sergei <sk_developer@mail.ru>
2023-11-05 15:14:13 +0300

    Сделана шапка экрана профиля

--- app/src/main/java/com/example/adaptumapp/presentation/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/adaptumapp/presentation/fragments/ProfileFragment.kt
@@ -1,20 +1,15 @@
 package com.example.adaptumapp.presentation.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.adaptumapp.R
+import androidx.fragment.app.Fragment
 import com.example.adaptumapp.databinding.FragmentProfileBinding
-import javax.inject.Inject
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
-
-//    @Inject
-//    lateinit var viewModel: ProfileFragmentViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,27 +19,4 @@ class ProfileFragment : Fragment() {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        (requireActivity().application as SynergySportApp).appComponent.inject(this)
-    }
-
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        bindViewModel()
-//        viewModel.init()
-
-
-//    private fun bindViewModel() {
-//        viewModel.profileInfoLiveData.observe(viewLifecycleOwner) {
-//            with(binding) {
-//                name.text = it.fullName
-//                telegram.text = it.telegram
-//                roleStrTv.text = it.roleStr
-//                companyNameTv.text = it.company
-//                mailTv.text = it.email
-//            }
-//        }
-//    }
 }
