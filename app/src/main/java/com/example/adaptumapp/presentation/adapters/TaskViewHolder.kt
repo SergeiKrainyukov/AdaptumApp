ee1aaaf53bb2ce1344a5ebe999021ba3b8c3df7a
Sergei <sk_developer@mail.ru>
2023-11-10 18:33:31 +0300

    Обновлен внешний вид карточек задач

--- app/src/main/java/com/example/adaptumapp/presentation/adapters/TaskViewHolder.kt
+++ app/src/main/java/com/example/adaptumapp/presentation/adapters/TaskViewHolder.kt
@@ -8,7 +8,6 @@ import com.example.adaptumapp.presentation.model.TaskListItem
 
 class TaskViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
     fun bind(training: TaskListItem) {
-        view.findViewById<TextView>(R.id.training_type_tv).text = training.type
-        view.findViewById<TextView>(R.id.training_date_tv).text = training.date
+        view.findViewById<TextView>(R.id.task_name_tv).text = training.type
     }
 }
